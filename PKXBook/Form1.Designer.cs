5dbf0b761e991dae1804c520080cee701640f5ab
Kurt <kaphotics@gmail.com>
2014-03-06 15:52:32 -0800

    Initial

---
+++ PKXBook/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PKXBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1040, 500);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(94, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(851, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(951, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "C BLK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(1005, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Extra";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 545);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PKXBook";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PKXBook/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace PKXBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] filepaths = new string[1];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                filepaths = Directory.GetFiles(fbd.SelectedPath, "*.*", SearchOption.AllDirectories);
+                textBox1.Text = fbd.SelectedPath;
+                dataGridView1.DoubleBuffered(true);
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+        }
+        private void loaddata()
+        {
+            this.dataGridView1.DataSource = null;
+            DataTable table = new DataTable();
+            table.Columns.Add("File", typeof(int));
+            for (int i = 0x90; i <= 0xAF; i++)
+            {
+                table.Columns.Add(i.ToString("X2"), typeof(string));
+            }
+            table.Columns.Add("Filename", typeof(string));
+
+            for (int i = 0; i < filepaths.Length; i++)
+            {
+                string path = filepaths[i];
+                string name = Path.GetFileNameWithoutExtension(path);
+                string ext = Path.GetExtension(path);
+                byte[] b = File.ReadAllBytes(path);
+                if ((ext == ".pk6") && (b.Length == 232))
+                {
+                    table.Rows.Add(i,
+                        b[0x90].ToString("X2"),
+                        b[0x91].ToString("X2"),
+                        b[0x92].ToString("X2"),
+                        b[0x93].ToString("X2"),
+                        b[0x94].ToString("X2"),
+                        b[0x95].ToString("X2"),
+                        b[0x96].ToString("X2"),
+                        b[0x97].ToString("X2"),
+                        b[0x98].ToString("X2"),
+                        b[0x99].ToString("X2"),
+                        b[0x9A].ToString("X2"),
+                        b[0x9B].ToString("X2"),
+                        b[0x9C].ToString("X2"),
+                        b[0x9D].ToString("X2"),
+                        b[0x9E].ToString("X2"),
+                        b[0x9F].ToString("X2"),
+                        b[0xA0].ToString("X2"),
+                        b[0xA1].ToString("X2"),
+                        b[0xA2].ToString("X2"),
+                        b[0xA3].ToString("X2"),
+                        b[0xA4].ToString("X2"),
+                        b[0xA5].ToString("X2"),
+                        b[0xA6].ToString("X2"),
+                        b[0xA7].ToString("X2"),
+                        b[0xA8].ToString("X2"),
+                        b[0xA9].ToString("X2"),
+                        b[0xAA].ToString("X2"),
+                        b[0xAB].ToString("X2"),
+                        b[0xAC].ToString("X2"),
+                        b[0xAD].ToString("X2"),
+                        b[0xAE].ToString("X2"),
+                        b[0xAF].ToString("X2"),
+                        name
+                        );
+                }
+            }
+
+            dataGridView1.DataSource = table;
+            for (int i = 0x0; i < 0x21; i++)
+            {
+                dataGridView1.Columns[i].Width = 23;
+            }
+            dataGridView1.Columns[0].Width = 30;
+            dataGridView1.Columns[0x21].Width = 200;
+
+        }
+        private void loaddata2()
+        {
+            this.dataGridView1.DataSource = null;
+            DataTable table2 = new DataTable();
+            table2.Columns.Add("File", typeof(int));
+
+            table2.Columns.Add(0x16.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x17.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x35.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x36.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x37.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x3A.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x3B.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x3C.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x3D.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x3E.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x3F.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x58.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x59.ToString("X2"), typeof(string));
+            table2.Columns.Add(0x73.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xC8.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xC9.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xCC.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xCD.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xCE.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xCF.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xD0.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xD7.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xE4.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xE5.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xE6.ToString("X2"), typeof(string));
+            table2.Columns.Add(0xE7.ToString("X2"), typeof(string));
+
+            table2.Columns.Add("Filename", typeof(string));
+
+            for (int i = 0; i < filepaths.Length; i++)
+            {
+                string path = filepaths[i];
+                string name = Path.GetFileNameWithoutExtension(path);
+                string ext = Path.GetExtension(path);
+                byte[] b = File.ReadAllBytes(path);
+                if ((ext == ".pk6") && (b.Length == 232))
+                {
+                    table2.Rows.Add(i,
+                        b[0x16].ToString("X2"),
+                        b[0x17].ToString("X2"),
+                        b[0x35].ToString("X2"),
+                        b[0x36].ToString("X2"),
+                        b[0x37].ToString("X2"),
+                        b[0x3A].ToString("X2"),
+                        b[0x3B].ToString("X2"),
+                        b[0x3C].ToString("X2"),
+                        b[0x3D].ToString("X2"),
+                        b[0x3E].ToString("X2"),
+                        b[0x3F].ToString("X2"),
+                        b[0x58].ToString("X2"),
+                        b[0x59].ToString("X2"),
+                        b[0x73].ToString("X2"),
+                        b[0xC8].ToString("X2"),
+                        b[0xC9].ToString("X2"),
+                        b[0xCC].ToString("X2"),
+                        b[0xCD].ToString("X2"),
+                        b[0xCE].ToString("X2"),
+                        b[0xCF].ToString("X2"),
+                        b[0xD0].ToString("X2"),
+                        b[0xD7].ToString("X2"),
+                        b[0xE4].ToString("X2"),
+                        b[0xE5].ToString("X2"),
+                        b[0xE6].ToString("X2"),
+                        b[0xE7].ToString("X2"),
+                        name
+                        );
+                }
+            }
+
+            dataGridView1.DataSource = table2;
+            for (int i = 1; i < 0x1c; i++)
+            {
+                dataGridView1.Columns[i].Width = 23;
+            }
+            dataGridView1.Columns[0].Width = 30;
+            dataGridView1.Columns[27].Width = 350;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loaddata();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            loaddata2();
+
+        }
+    }
+    public static class ExtensionMethods    // Speed up scrolling
+    {
+        public static void DoubleBuffered(this DataGridView dgv, bool setting)
+        {
+            Type dgvType = dgv.GetType();
+            PropertyInfo pi = dgvType.GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+            pi.SetValue(dgv, setting, null);
+        }
+    }
+}
